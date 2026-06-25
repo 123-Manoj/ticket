b0b53dcf5d30aa34acb8ad5750bff4c6a3bc3f3e
E V Manoj <evmanoj444@gmail.com>
2026-06-25 22:41:18 +0530

    added tour.java

---
+++ tour.java
@@ -0,0 +1,12 @@
+class ticket
+{
+  public static void main(String srgs[])
+    { 
+       int child,adult,amount,total;
+       amount=1200;
+       adult=12*amount;
+       child=18*amount/2;
+       total=child+adult;
+       System.out.println("total"+"="+total);
+   }
+}
